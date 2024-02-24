4d59565f7e8b2443cb3ef0366e3e79010846f600
NGOMA SITA <dorian.ngoma-sita@capgemini.com>
2024-02-24 19:50:03 +0100

    fitre ville

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,17 +11,27 @@ import { AuthService } from '@auth0/auth0-angular';
 export class HomeComponent {
 
   restaurants: any[]=[];
+  restaurants_par_ville: any[]=[];
   initialRestaurants: any[]=[];
   ngOnInit(): void {
     this.http.get<any>('https://fooddeliverapi.vercel.app/api/restaurants')
       .subscribe(response => {
         this.restaurants = response;
         this.initialRestaurants=response
-        console.log(response); // Mettez le console.log ici pour s'assurer qu'il est exécuté après la réception des données.
+        this.restaurants_par_ville=response
+        console.log(response); // assurer qu'il est exécuté après la réception des données.
       });
   }
 
   constructor(public auth: AuthService,private http: HttpClient,private router: Router) { }
+
+  filtre_ville(event: any) {
+    const ville_select = (event.target as HTMLSelectElement).value;
+    console.log(ville_select)
+    const result = this.SearchRestaurants(ville_select,2,this.initialRestaurants);
+    this.restaurants = result;
+    this.restaurants_par_ville=result;
+  }
 
   goToRestaurantDetails(restaurant:any): void {
     this.router.navigate(['/restaurant'], { queryParams: { id: restaurant.id } });
@@ -32,21 +42,25 @@ export class HomeComponent {
     const searchTerm = inputRecherche.value.trim();
     if (searchTerm === '') {
       // Si l'entrée est vide, réinitialiser la liste des restaurants
-      this.restaurants = this.initialRestaurants; // Réinitialiser à la liste initiale
+      this.restaurants = this.restaurants_par_ville; // Réinitialiser à la liste initiale
   } else {
-      const result = this.SearchRestaurants(searchTerm);
-   
+      const result = this.SearchRestaurants(searchTerm,1,this.restaurants_par_ville);
+
       this.restaurants = result;
   }
 
   }
   // Fonction pour effectuer une recherche  dans la liste des restaurants
-  SearchRestaurants(query: string) {
+  SearchRestaurants(query: string,filtre:number,restaurants: any[]) {
     query = query.toLowerCase();
-    console.log(query)
-    return this.initialRestaurants.filter(restaurant => {
-        const nomRestaurant = restaurant.nom.toLowerCase();
-        // Algorithme de recherche fuzzy simple
+    return restaurants.filter(restaurant => {
+        var nomRestaurant = restaurant.nom.toLowerCase();
+        
+        if(filtre==2){
+          nomRestaurant = restaurant.ville.toLowerCase();
+        }
+        
+        // Algorithme de recherche  simple
         let score = 0;
         for (let i = 0; i < query.length; i++) {
             const index = nomRestaurant.indexOf(query[i]);
